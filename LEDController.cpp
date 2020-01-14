aa3054709a7923c8121a124674169b39ed4619e4
gabe <gabe>
2020-01-14 20:59:02 +0000

    Use client MAC as id'

--- LEDController.cpp
+++ LEDController.cpp
@@ -1,3 +1,6 @@
+#include <sstream>
+#include <iomanip>
+
 #include "LEDController.h"
 #include "LEDServer.h"
 #include <boost/log/trivial.hpp>
@@ -33,11 +36,10 @@ LEDController& LEDController::operator=(LEDController&& c) {
 }
 
 void LEDController::read_header() {
-  async_read(sock_, buffer(&id_, sizeof(id_)),
+  async_read(sock_, buffer(id_, sizeof(id_)),
 	     [this](const std::error_code& ec, std::size_t bytes) {
 	       if (!ec && bytes) {
-		 id_ = ntohl(id_);
-		 LOG(info) << "Header: ID = 0x" << std::hex << id_;
+		 LOG(info) << "Header: ID = " << id_str();
 		 read_header();
 	       } else {
 		 LOG(error) << "Read Error: " << ec.message();
@@ -51,7 +53,7 @@ void LEDController::send(const const_buffer& buf) {
   async_write(sock_, buf,
 	      [this](const std::error_code& ec, std::size_t bytes) {
 		if (!ec) {
-		  LOG(info) << bytes << " bytes sent to client ID " << std::hex << ntohl(id_)
+		  LOG(info) << bytes << " bytes sent to client ID " << id_str()
 			    << " in " << std::dec
 			    << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start_).count() << "ms";
 		} else {
@@ -66,7 +68,7 @@ void LEDController::send_sync() {
   async_write(sock_, buffer("SYNC", 4),
 	      [this](const std::error_code& ec, std::size_t bytes) {
 		if (!ec) {
-		  LOG(info) << "SYNC sent to " << std::hex << ntohl(id_);
+		  LOG(info) << "SYNC sent to " << id_str();
 		} else {
 		  LOG(error) << "Write Error: " << ec.message();
 		  server_.get().post_connection_error(*this);
@@ -74,3 +76,14 @@ void LEDController::send_sync() {
 	      });  
 }
 
+std::string LEDController::id_str() {
+  std::stringstream ss;
+  ss << std::setfill('0') << std::hex
+     << std::setw(2) << static_cast<int>(id_[0]) << "-"
+     << std::setw(2) << static_cast<int>(id_[1]) << "-"
+     << std::setw(2) << static_cast<int>(id_[2]) << "-"
+     << std::setw(2) << static_cast<int>(id_[3]) << "-"
+     << std::setw(2) << static_cast<int>(id_[4]) << "-"
+     << std::setw(2) << static_cast<int>(id_[5]);
+  return ss.str();
+}
--- LEDController.h
+++ LEDController.h
@@ -15,9 +15,10 @@ public:
   ~LEDController();
   bool operator==(const LEDController& c) { return id_ == c.id_; }
   LEDController& operator=(LEDController&& c);
-  uint32_t id() { return id_; }
+  const uint8_t* id() { return id_; }
   void send(const boost::asio::const_buffer& buf);
   void send_sync();
+  std::string id_str();
   
 private:
 
@@ -26,7 +27,7 @@ private:
   std::reference_wrapper<LEDServer> server_;
   boost::asio::ip::tcp::socket sock_;
   std::reference_wrapper<boost::asio::io_context> io_;
-  uint32_t id_;
+  uint8_t id_[6];
   std::chrono::steady_clock::time_point start_;
 };
   
